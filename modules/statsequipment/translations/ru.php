036a9662ca81e20294c9b17931a1ecac313acb0c
Invince <markijan9891@gmail.com>
2014-11-26 15:38:03 +0200

    irst`

---
+++ modules/statsequipment/translations/ru.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{statsequipment}prestashop>statsequipment_247270d410e2b9de01814b82111becda'] = 'Браузеры и операционные системы';
+$_MODULE['<{statsequipment}prestashop>statsequipment_2876718a648dea03aaafd4b5a63b1efe'] = 'Добавляет вкладку, содержащую графики о браузерах и операционных системах в статистику панели инструментов.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_6602bbeb2956c035fb4cb5e844a4861b'] = 'Руководство';
+$_MODULE['<{statsequipment}prestashop>statsequipment_0d5f13106dec10bb8a9301541052278c'] = 'Управляя сайтами, важно знать, какое ПО используют посетители. Это позволит обеспечить одинаковое отображение сайта для всех. PrestaShop разрабатывается совместимым с современными веб-браузерами и ОС. Однако, когда вы добавляете новые функции на вашем сайте или даже модифицируете ядро PrestaShop, эти изменения могут не быть доступными для всех. Вот почему полезно заранее знать, какой части пользователей, использующих разные браузеры, будут доступны эти модификации.';
+$_MODULE['<{statsequipment}prestashop>statsequipment_998e4c5c80f27dec552e99dfed34889a'] = 'Export CSV';
+$_MODULE['<{statsequipment}prestashop>statsequipment_bb38096ab39160dc20d44f3ea6b44507'] = 'Плагины';
+
+
+return $_MODULE;
